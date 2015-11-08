c6bde03dbfbab874352d5f3e6f36d3aac7980b6b
Leonardy Kristianto <alendory@gmail.com>
2015-11-08 11:42:46 +0800

    1 hour 17 mins left

--- client/app.jsx
+++ client/app.jsx
@@ -171,7 +171,7 @@ Detail = React.createClass({
           <div className="col-xs-8 col-xs-offset-4 beer-info">
             <div className="flavour">
               <h4>Flavour</h4><br/>
-              <span className="label label-primary">Fruity</span>
+              <span style={{color: '#3f120d'}}>Fruity</span>
             </div>
             <div className="pairings">
               <h4>Best paired with</h4>
@@ -226,21 +226,21 @@ ThumbScore = React.createClass({
 Ready = React.createClass({
   getUsers() {
     var users = [
-      { _id: 0001, name: 'Stevey', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player player-one' },
-      { _id: 0002, name: 'Phoebe', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player player-two' },
-      { _id: 0003, name: 'Bud', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player player-three' },
-      { _id: 0004, name: 'Bagnew', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player player-four'  },
-      { _id: 0005, name: 'Gazza', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player player-five'  }
+      { _id: 0001, name: 'Stevey', pictureURL: 'assets/profiles/1.jpg', score: 0, class: 'player player-one' },
+      { _id: 0002, name: 'Phoebe', pictureURL: 'assets/profiles/2.jpg', score: 0, class: 'player player-two' },
+      { _id: 0003, name: 'Bud', pictureURL: 'assets/profiles/3.jpg', score: 0, class: 'player player-three' },
+      { _id: 0004, name: 'Bagnew', pictureURL: 'assets/profiles/4.jpg', score: 0, class: 'player player-four'  },
+      { _id: 0005, name: 'Gazza', pictureURL: 'assets/profiles/5.jpg', score: 0, class: 'player player-five'  }
     ]
     return users;
   },
   getUser(index, addClass) {
     var users = [
-      { _id: 0001, name: 'Stevey', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass },
-      { _id: 0002, name: 'Phoebe', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass },
-      { _id: 0003, name: 'Bud', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass },
-      { _id: 0004, name: 'Bagnew', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass  },
-      { _id: 0005, name: 'Gazza', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass  }
+      { _id: 0001, name: 'Stevey', pictureURL: 'assets/profiles/1.jpg', score: 0, class: 'player ' + addClass },
+      { _id: 0002, name: 'Phoebe', pictureURL: 'assets/profiles/2.jpg', score: 0, class: 'player ' + addClass },
+      { _id: 0003, name: 'Bud', pictureURL: 'assets/profiles/3.jpg', score: 0, class: 'player ' + addClass },
+      { _id: 0004, name: 'Bagnew', pictureURL: 'assets/profiles/4.jpg', score: 0, class: 'player ' + addClass  },
+      { _id: 0005, name: 'Gazza', pictureURL: 'assets/profiles/5.jpg', score: 0, class: 'player ' + addClass  }
     ];
     return users[index];
   },
@@ -263,9 +263,8 @@ Ready = React.createClass({
           <div className="container">
             <div className="waiting-room-players">
               <div className="main-player">
-                <img src="../assets/sample_profile.jpg" />
-                <br />
-                <span>Leonardy</span>
+                <span className="timer"><CountdownTimer secondsRemaining="60"/></span><br/>
+                <span className="timer-seconds">secs</span>
               </div>
               {this.renderThumbnail(0, 'player-one')}
               {this.renderThumbnail(1, 'player-two')}
@@ -291,14 +290,114 @@ Ready = React.createClass({
   }
 });
 
+CountdownTimer = React.createClass({
+  getInitialState: function() {
+    return {
+      secondsRemaining: 0
+    };
+  },
+  tick: function() {
+    this.setState({ secondsRemaining: this.state.secondsRemaining - 1});
+    if (this.state.secondsRemaining <= 0) {
+      clearInterval(this.interval);
+    }
+  },
+  componentDidMount: function() {
+    this.setState({ secondsRemaining: this.props.secondsRemaining });
+    this.interval = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+      <span>{this.state.secondsRemaining}</span>
+    );
+  }
+});
+
+FirstWord = React.createClass({
+  getInitialState: function() {
+    return {
+      word: ''
+    };
+  },
+  change: function() {
+    this.setState({ word: Session.get("firstWord") });
+  },
+  componentDidMount: function() {
+    this.setState({ word: Session.get("firstWord") });
+    this.interval = setInterval(this.change, 100);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+      <span className="fillers">{this.state.word}</span>
+    );
+  }
+});
+
+SecondWord = React.createClass({
+  getInitialState: function() {
+    return {
+      word: ''
+    };
+  },
+  change: function() {
+    this.setState({ word: Session.get("secondWord") });
+  },
+  componentDidMount: function() {
+    this.setState({ word: Session.get("secondWord") });
+    this.interval = setInterval(this.change, 100);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+      <span className="fillers">{this.state.word}</span>
+    );
+  }
+});
+
+Label = React.createClass({
+  propTypes: {
+    params: React.PropTypes.object.isRequired
+  },
+  getInitialState: function() {
+    return {
+      // stuff
+    }
+  },
+  class: function(type) {
+    return "label label-" + type;
+  },
+  setWord: function() {
+    // console.log(this.props.params.name);
+    // Session.set("firstWord", this.props.params.name);
+    if (Session.equals("firstWord", undefined)) {
+      Session.set("firstWord", this.props.params.name);
+    } else {
+      Session.set("secondWord", this.props.params.name);
+    }
+  },
+  render: function() {
+    return (
+      <div className={this.class(this.props.params.type)} onClick={this.setWord}>{this.props.params.name}</div>
+    );
+  }
+});
+
 Game = React.createClass({
   getUser(index, addClass) {
     var users = [
-      { _id: 0001, name: 'Stevey', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass },
-      { _id: 0002, name: 'Phoebe', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass },
-      { _id: 0003, name: 'Bud', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass },
-      { _id: 0004, name: 'Bagnew', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass  },
-      { _id: 0005, name: 'Gazza', pictureURL: 'assets/sample_profile.jpg', score: 0, class: 'player ' + addClass  }
+      { _id: 0001, name: 'Stevey', pictureURL: 'assets/profiles/1.jpg', score: 18, class: 'player ' + addClass },
+      { _id: 0002, name: 'Phoebe', pictureURL: 'assets/profiles/2.jpg', score: 12, class: 'player ' + addClass },
+      { _id: 0003, name: 'Bud', pictureURL: 'assets/profiles/3.jpg', score: 11, class: 'player ' + addClass },
+      { _id: 0004, name: 'Bagnew', pictureURL: 'assets/profiles/4.jpg', score: 9, class: 'player ' + addClass  },
+      { _id: 0005, name: 'Gazza', pictureURL: 'assets/profiles/5.jpg', score: 7, class: 'player ' + addClass  }
     ]
     return users[index];
   },
@@ -308,13 +407,56 @@ Game = React.createClass({
       <ThumbScore key={user._id} thumbnail={user} />
     );
   },
+  getLabels: function() {
+    var labels = [
+      { _id: "001", type: 'primary', name: 'Fruity' },
+      { _id: "002", type: 'foodpairing', name: 'Artichokes' },
+      { _id: "003", type: 'foodpairing', name: 'Chicken' },
+      { _id: "004", type: 'foodpairing', name: 'Ginger' },
+      { _id: "005", type: 'foodpairing', name: 'Fudge' },
+      { _id: "006", type: 'foodpairing', name: 'Caramelised' },
+      { _id: "007", type: 'default', name: 'Sexy' },
+      { _id: "008", type: 'default', name: 'Balls' },
+      { _id: "009", type: 'default', name: 'Shiok' },
+      { _id: "010", type: 'default', name: 'Mankini' },
+      { _id: "011", type: 'default', name: 'Aliens' },
+      { _id: "012", type: 'default', name: 'Drunken' },
+      { _id: "013", type: 'default', name: 'Zombies' },
+      { _id: "014", type: 'default', name: 'XL' },
+      { _id: "015", type: 'default', name: 'Suck' },
+    ];
+    return labels;
+  },
+  renderLabels: function() {
+    return this.getLabels().map((doc) => {
+      return <Label key={doc._id} params={doc} />;
+    });
+  },
+  getCountdown() {
+    // var countdown = new ReactiveCountdown(5);
+    // countdown.start(function() {
+    //   console.log("done?");
+    // });
+    // return countdown.get();
+  },
+  getFirstWord: function() {
+    // Session.set("firstWord", "Artichokes");
+    return Session.get("firstWord");
+  },
+  getSecondWord: function() {
+    // Session.set("secondWord", "Balls");
+    return Session.get("secondWord");
+  },
+  setWord(doc) {
+    console.log(doc);
+  },
   render() {
     return (
       <div>
         <div className="game">
           <div className="container player-bar">
-            {this.renderThumbscore(0, '')}
-            {this.renderThumbscore(1, 'active')}
+            {this.renderThumbscore(0, 'active')}
+            {this.renderThumbscore(1, '')}
             {this.renderThumbscore(2, '')}
             {this.renderThumbscore(3, '')}
             {this.renderThumbscore(4, '')}
@@ -322,17 +464,19 @@ Game = React.createClass({
           <div className="container the-word">
             <div className="hero">
               <div className="hero-text">
-                <h1>LIFE IS BETTER WHEN IT'S FULL OF <span className="fillers"></span><span className="fillers"></span>.</h1>
+                <h1>LIFE IS BETTER WHEN IT'S FULL OF <FirstWord word={this.getFirstWord()}/><SecondWord word={this.getSecondWord()}/>.</h1>
               </div>
             </div>
             <h4 className="text-center">Pick 2 words to fill the blanks.</h4>
-            <p>
-              <span className="label label-primary">Fruity</span> <span className="label label-foodpairing">Artichokes</span> <span className="label label-foodpairing">Chicken</span> <span className="label label-default">Balls</span> <span className="label label-default">Sexy</span>
-            </p>
+            {this.renderLabels()}
           </div>
-          <div className="timer">00:30</div>
+          <div className="timer">
+            <CountdownTimer secondsRemaining="60"/>
+          </div>
           <div className="container next-button-fixed">
+            <Link to="/judge">
             <button className="btn btn-primary btn-block btn-lg">Waiting for other players <i className="fa fa-spinner fa-spin fa-fw"></i></button>
+            </Link>
           </div>
         </div>
       </div>
@@ -341,6 +485,22 @@ Game = React.createClass({
 });
 
 Judge = React.createClass({
+  getUser(index, addClass) {
+    var users = [
+      { _id: 0001, name: 'Stevey', pictureURL: 'assets/profiles/1.jpg', score: 18, class: 'player ' + addClass },
+      { _id: 0002, name: 'Phoebe', pictureURL: 'assets/profiles/2.jpg', score: 12, class: 'player ' + addClass },
+      { _id: 0003, name: 'Bud', pictureURL: 'assets/profiles/3.jpg', score: 11, class: 'player ' + addClass },
+      { _id: 0004, name: 'Bagnew', pictureURL: 'assets/profiles/4.jpg', score: 9, class: 'player ' + addClass  },
+      { _id: 0005, name: 'Gazza', pictureURL: 'assets/profiles/5.jpg', score: 7, class: 'player ' + addClass  }
+    ]
+    return users[index];
+  },
+  renderThumbscore(index, addClass) {
+    var user = this.getUser(index, addClass);
+    return (
+      <ThumbScore key={user._id} thumbnail={user} />
+    );
+  },
   render() {
     return (
       <div>
@@ -363,43 +523,38 @@ Judge = React.createClass({
                 <div className="input-group">
                   <input id="option" type="radio" name="field" value="option"/>
                   <label for="option">
-                    <span className="label label-foodpairing">Caramelised</span>
-                    <span className="label label-default">Balls</span>
+                    <span className="label label-foodpairing"><FirstWord/></span>&nbsp;
+                    <span className="label label-default"><SecondWord/></span>
                   </label>
                   <div className="score-notification play-score-outro">+3</div>
                 </div>
                 <div className="input-group">
                   <input id="option" type="radio" name="field" value="option"/>
                   <label for="option">
-                    <span className="label label-foodpairing">Ginger</span>
+                    <span className="label label-foodpairing">Ginger</span>&nbsp;
                     <span className="label label-default">Aliens</span>
                   </label>
                 </div>
                 <div className="input-group">
                   <input id="option" type="radio" name="field" value="option"/>
                   <label for="option">
-                    <span className="label label-primary">Fruity</span>
+                    <span className="label label-primary">Fruity</span>&nbsp;
                     <span className="label label-default">Zombies</span>
                   </label>
                 </div>
                 <div className="input-group">
                   <input id="option" type="radio" name="field" value="option"/>
                   <label for="option">
-                    <span className="label label-default">Hairy</span>
+                    <span className="label label-default">Hairy</span>&nbsp;
                     <span className="label label-default">Grandmas</span>
-                  </label>
-                </div>
-                <div className="input-group">
-                  <input id="option" type="radio" name="field" value="option"/>
-                  <label for="option">
-                    <span className="label label-default">Charming</span>
-                    <span className="label label-default">Badger</span>
                   </label>
                 </div>
               </div>
             </div>
-            <div className="container next-button-fixed">
-              <button className="btn btn-primary btn-block btn-lg">Next Round</button>
+            <div className="container">
+              <Link to="/gameEnd">
+                <button className="btn btn-primary btn-block btn-lg">Next Round</button>
+              </Link>
             </div>
         </div>
       </div>
@@ -411,7 +566,79 @@ GameEnd = React.createClass({
   render() {
     return (
       <div>
-
+        <div className="game game-over">
+          <div className="container the-word">
+            <div className="hero">
+              <div className="hero-text">
+                <h1>Total Scores</h1>
+                <h2>Game Over!</h2>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row selected">
+              <div className="col-xs-12 player-summary-outer">
+                <div className="player-summary">
+                  <img src="../assets/profiles/1.jpg" />
+                  <h5>Stevey</h5>
+                  <div className="scoring">Winner!<span className="label label-primary">18</span></div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 player-summary-outer">
+                <div className="player-summary">
+                  <img src="../assets/profiles/2.jpg" />
+                  <h5>Phoebe</h5>
+                  <div className="scoring"><span className="label label-default">12</span></div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 player-summary-outer">
+                <div className="player-summary">
+                  <img src="../assets/profiles/3.jpg" />
+                  <h5>Bud</h5>
+                  <div className="scoring"><span className="label label-default">11</span></div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 player-summary-outer">
+                <div className="player-summary">
+                  <img src="../assets/profiles/4.jpg" />
+                  <h5>Bagnew</h5>
+                  <div className="scoring"><span className="label label-default">9</span></div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 player-summary-outer">
+                <div className="player-summary">
+                  <img src="../assets/profiles/5.jpg" />
+                  <h5>Gazza</h5>
+                  <div className="scoring"><span className="label label-default">7</span></div>
+                </div>
+              </div>
+            </div>
+            <div className="row scoring-footer">
+              <div className="col-xs-12">
+                <p>Register your name and mobile number to save your points and continue to play for more rewards!</p>
+                <div className="row scoring-footer-inputs">
+                  <div className="col-xs-6">
+                    <input type="text" className="form-control" placeholder="Name" />
+                  </div>
+                  <div className="col-xs-6">
+                    <input type="text" className="form-control" placeholder="Contact" />
+                  </div>
+                </div>
+                <Link to="/selection">
+                  <button className="btn btn-primary btn-block btn-lg">Start another game</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -443,6 +670,8 @@ Routes = React.createClass({
     );
   }
 });
+
+
 
 // Rendering
 Meteor.startup(function() {
